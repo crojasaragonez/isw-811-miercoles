f4e98ce72476f0c1c80ae5d88cbd2c706ae5e72b
roberto29 <roberto_aramu@hotmail.com>
2017-07-24 20:30:12 -0600

    routes

--- clicks/routes/sessions.js
+++ clicks/routes/sessions.js
@@ -8,7 +8,13 @@ var criteria = function(req) {
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-res.json({count: 1 });
+res.send(req.session.count);
+
+});
+
+router.post('/', function(req, res, next){
+	req.session.count = req.body.count;
+	res.send(req.session.count);
 });
 
 module.exports = router;
